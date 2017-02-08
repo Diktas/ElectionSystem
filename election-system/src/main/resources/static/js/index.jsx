074db6723e81a4060b715ca79c4c28de9eca1e38
Diktas <g.simanelis@gmail.com>
2017-02-08 18:30:53 +0200

    react mainpage

--- election-system/src/main/resources/static/js/index.jsx
+++ election-system/src/main/resources/static/js/index.jsx
@@ -1,17 +1,17 @@
-var App = React.createClass({
-  render: function() {
-    return (
-      <div style={{ paddingTop: '20px' }}>
-        {this.props.children}
-      </div>
-    );
-  }
+var App = React.createClass( {
+    render: function() {
+        return (
+            <div style={{ paddingTop: '20px' }}>
+                {this.props.children}
+            </div>
+        );
+    }
 });
 
-var NoMatch = React.createClass({
-  render: function() {
-    return <div>Route did not match</div>;
-  }
+var NoMatch = React.createClass( {
+    render: function() {
+        return <div>Route did not match</div>;
+    }
 });
 
 var Router = ReactRouter.Router;
@@ -20,11 +20,11 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 
 ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={HelloWorldComponent} />
-      <Route path="/hello-world" component={HelloWorldComponent} />
-      <Route path="*" component={NoMatch}/>
-    </Route>
-  </Router>
-), document.getElementById('root'));
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={MainPageContainer} />
+            <Route path="/hello-world" component={HelloWorldComponent} />
+            <Route path="*" component={NoMatch} />
+        </Route>
+    </Router>
+), document.getElementById( 'root' ) );
